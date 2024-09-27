ad88826b5a69949e94dac0a3c5848c3ff70f1ccb
JiAyi <160787619+JiAyiX-11@users.noreply.github.com>
2024-09-27 02:12:50 +0200

    Add files via upload

---
+++ 2_install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cat zshrc.txt > ~/.zshrc
+
+source ~/.zshrc
