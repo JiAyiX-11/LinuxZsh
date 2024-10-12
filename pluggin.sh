6117267cf2f47258f0f98035bbaf4b960019848c
JiAyi <160787619+JiAyiX-11@users.noreply.github.com>
2024-10-13 01:36:54 +0200

    Update pluggin.sh

--- pluggin.sh
+++ pluggin.sh
@@ -2,5 +2,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+
+git clone https://github.com/zdharma-continuum/alias-finder ~/.oh-my-zsh/custom/plugins/alias-finder
+
 cat zshrc.txt > ~/.zshrc
 
